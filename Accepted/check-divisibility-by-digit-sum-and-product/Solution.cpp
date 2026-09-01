542ece9204deac64acefada08639b97b18cb14a4
vato <shrivathsa1551@gmail.com>
2026-09-01 22:51:28 +0530

    Sync Solution: check-divisibility-by-digit-sum-and-product

---
+++ Accepted/check-divisibility-by-digit-sum-and-product/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int x = n;
+        int digitSum = 0;
+        int digitProduct = 1;
+
+        while (x > 0) {
+            int digit = x % 10;
+
+            digitSum += digit;
+            digitProduct *= digit;
+
+            x /= 10;
+        }
+
+        int divisor = digitSum + digitProduct;
+
+        return n % divisor == 0;
+    }
+};
