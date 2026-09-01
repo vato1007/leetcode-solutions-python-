38f4925b32f81b887d37b328710e6f5ad4ac4335
vato <shrivathsa1551@gmail.com>
2026-09-01 22:55:05 +0530

    Sync Solution: robot-return-to-origin

---
+++ Accepted/robot-return-to-origin/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves):
+        x = 0
+        y = 0
+
+        for move in moves:
+            if move == 'R':
+                x += 1
+            elif move == 'L':
+                x -= 1
+            elif move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+
+        return x == 0 and y == 0
